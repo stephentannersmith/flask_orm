d489446e20e96972a74aa35764db17ca9f38444a
Tanner Smith <tanner.ssu@gmail.com>
2020-02-23 11:50:50 -0600

    organized a bit better

--- oop_demo/bank_account.py
+++ oop_demo/bank_account.py
@@ -2,27 +2,27 @@ class User:
     def __init__(self, username, email_address):
         self.name = username
         self.email = email_address
-        self.account_balance = 0
+        self.account = Bank_account(int_rate=0.02, account_balance=0)
 
     def make_deposit(self, amount):
-        self.account_balance += amount
+        self.account.deposit
         return self
 
     def make_withdrawal(self, amount):
-        self.account_balance -= amount
+        self.account.withdraw
         return self 
 
-    def display_user_balance(self):
-        print(f"{self.name}: {self.account_balance}")
+    def display_user_info(self):
+        print(f"{self.name}: {self.account.balance}")
         return self
 
     def transfer_amount(self, target, amount):
-        self.make_withdrawal(amount)
-        target.make_deposit(amount)
+        self.account.withdraw(amount)
+        target.account.deposit(amount)
         return self
 
 class Bank_account:
-    def __init__(self, account_balance=0):
+    def __init__(self, int_rate=0, account_balance=0):
         self.int_rate = 0.01
         self.balance = account_balance
         
@@ -43,6 +43,6 @@ class Bank_account:
         return self
     
 if __name__ == "__main__":
-    Tanner = Bank_account(400).deposit(30).deposit(100).deposit(100).withdraw(100).yield_interest().display_account_info()
+    Tanner = Bank_account(400).deposit(30).deposit(100).deposit(100).withdraw(100).transfer_amount(Ryan, 100).yield_interest().display_account_info()
     Ryan = Bank_account(200).deposit(100).deposit(200).withdraw(100).withdraw(50).withdraw(100).withdraw(10).yield_interest().display_account_info()
     
